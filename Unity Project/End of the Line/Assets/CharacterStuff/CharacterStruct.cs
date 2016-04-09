826a8866732b716d0e416989c1aa30d85f8903b8
Joseph Brommel <joseph.brommel@gmail.com>
2016-04-08 23:44:36 -0500

    Characterstruct is in here. That's all that's added.

---
+++ Unity Project/End of the Line/Assets/CharacterStuff/CharacterStruct.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; //maybe?
+using System.Web.Scripts
+
+public class CharacterStruct {
+	
+	public string name;
+	public string type; //student, animal, politician, etc.
+	public string sprite; //takes in the person's png
+	private Dictionary<string, string> data;
+
+	void parseJSON(string jsonString) { //parses a JSON file and turns it into a type character struct
+		var json = new JavaScriptSerializer();
+		data = json.Deserialize<Dictionary<string, string>> (jsonString);
+		data.TryGetValue ("name", out name); // tries to access the dictionary looking for the key name, then puts it in the variable name
+		data.TryGetValue("type", out type); // tries to access the dictionary looking for the key name, then puts it in the variable name
+		data.TryGetValue("sprite", out sprite); // tries to access the dictionary looking for the key name, then puts it in the variable name
+	}
+}
