e9c4ede872390cdf7af13863111ae773a4b3ce67
Charlie Imhoff <cpimhoff@gmail.com>
2016-04-08 23:50:02 -0500

    patch generation script

---
+++ Unity Project/End of the Line/Assets/World Generation/Generation/PatchGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatchGenerator : MonoBehaviour {
+
+	public List<GameObject> templatePatches = new List<GameObject>();
+
+	// Returns a new patch for the world
+	GameObject NextPatch() {
+
+		// get a random patch type
+		int randIndex = Random.Range (0, templatePatches.Count);
+		GameObject template = GameObject.Instantiate (templatePatches [randIndex]);
+
+		// replace objects tagged with ReplaceWithCharacter with characters
+
+
+		// return template (now properly constructed)
+		return template;
+	}
+
+}
