71dece8ce219f0f6cd059f1e465552d1fcfb73dc
Charlie Imhoff <cpimhoff@gmail.com>
2016-04-09 06:02:28 -0500

    Train follows Rails
    Next up, Level Action!

---
+++ Unity Project/End of the Line/Assets/World Generation/Parts/Script/RayGizmo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayGizmo : MonoBehaviour {
+
+	public Transform origin;
+	public Transform destination;
+
+	public Vector3 direction {
+		get {
+			return destination.position - origin.position;
+		}
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.blue;
+		Ray ray = new Ray(origin.position, direction);
+		Gizmos.DrawRay (ray);
+	}
+}
