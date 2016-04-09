88a7675f3f688d2f8869e45520c5b14d6cb8e3ef
Charlie Imhoff <cpimhoff@gmail.com>
2016-04-09 04:07:08 -0500

    The train on the rails, world patches generate when you exit the bounds of the patch

---
+++ Unity Project/End of the Line/Assets/Primary Gameplay/Script/PatchManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Manages the generation, replacement, and transition between world Patches
+/// </summary>
+public class PatchManager : MonoBehaviour {
+
+	public PatchGenerator generator;	// the generator to use for creating patches
+	public GameObject trackedObject;	// the object which will trigger patch generation by moving close to off grid.
+
+	private GameObject currentPatch;	// the current patch
+	private Bounds patchRegion // the bounding box of the current patch
+	{	get { return  currentPatch.GetComponent<BoxCollider>().bounds; } }
+
+	// Use this for initialization
+	void Start () {
+		this.currentPatch = generator.NextPatch ();
+
+		this.currentPatch.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (patchRegion.Contains (trackedObject.transform.position)) {
+			// the tracked object is within the patch's region	
+		} else {
+			// generate the template (populated)
+			GameObject newPatch = generator.NextPatch ();
+
+			// rotate the new patch to match the yaw of the tracked object
+			newPatch.transform.rotation = this.trackedObject.transform.rotation;
+
+			// move the tracked object to the start of the new patch
+			var trainPoint = newPatch.GetComponent<PatchMeta> ().trainPlacementPoint.position;
+			this.trackedObject.transform.position = trainPoint;
+
+			// replace old patch
+			newPatch.SetActive (true);
+			Destroy(this.currentPatch.gameObject);
+			this.currentPatch = newPatch;
+		}
+	}
+
+}
---
+++ Unity Project/End of the Line/Assets/Primary Gameplay/Script/TrainMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Moves a Train realistically up to a max speed.
+/// </summary>
+public class TrainMovement : MonoBehaviour {
+
+	public float speed = 1.5f;
+	public float maxVelocityMagnitude = 10;
+
+	private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+		this.body = this.GetComponent<Rigidbody> ();
+	}
+
+	// FixedUpdate is called once per physics frame
+	void FixedUpdate () {
+		Vector3 force = new Vector3 (0, 0, speed);
+		body.AddRelativeForce (force, ForceMode.Acceleration);
+
+		if (body.velocity.sqrMagnitude >= (maxVelocityMagnitude*maxVelocityMagnitude)) {
+			Vector3 normalled = body.velocity.normalized;
+			Vector3 newVelocity = new Vector3 (body.velocity.x, body.velocity.y, body.velocity.z);
+			newVelocity.x = normalled.x * maxVelocityMagnitude;
+			newVelocity.y = normalled.y * maxVelocityMagnitude;
+			newVelocity.z = normalled.z * maxVelocityMagnitude;
+			body.velocity = newVelocity;
+		}
+	}
+}
