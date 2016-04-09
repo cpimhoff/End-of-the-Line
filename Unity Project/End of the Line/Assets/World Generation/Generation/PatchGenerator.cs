b2133804642e1349a98cfa2cc74c62b3c175fb4b
Charlie Imhoff <cpimhoff@gmail.com>
2016-04-09 01:22:14 -0500

    Patch generation and character replacement now working as prototype

--- Unity Project/End of the Line/Assets/World Generation/Generation/PatchGenerator.cs
+++ Unity Project/End of the Line/Assets/World Generation/Generation/PatchGenerator.cs
@@ -1,22 +1,32 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class PatchGenerator : MonoBehaviour {
 
+	public GameObject testCharacter;
 	public List<GameObject> templatePatches = new List<GameObject>();
 
-	// Returns a new patch for the world
-	GameObject NextPatch() {
-
+	// Returns a new patch for the world (these start as inactive!)
+	public GameObject NextPatch() {
 		// get a random patch type
 		int randIndex = Random.Range (0, templatePatches.Count);
-		GameObject template = GameObject.Instantiate (templatePatches [randIndex]);
+		GameObject patch = GameObject.Instantiate (templatePatches [randIndex]);
 
 		// replace objects tagged with ReplaceWithCharacter with characters
+		GameObject[] replaceThese = GameObject.FindGameObjectsWithTag("ReplaceWithCharacter");
+		foreach (GameObject replaceMe in replaceThese) {
+			//new character at the location of 'replaceMe'
+			GameObject character = GameObject.Instantiate (testCharacter);
+			character.transform.SetParent (patch.transform);
+			character.transform.position = replaceMe.transform.position;
 
+			replaceMe.tag = "Untagged";
+			replaceMe.SetActive (false);
+		}
 
 		// return template (now properly constructed)
-		return template;
+		patch.SetActive (false);
+		return patch;
 	}
 
 }
