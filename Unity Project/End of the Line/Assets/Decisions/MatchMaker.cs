38993ec228f8b56ab3454b30357fe35907dec03e
Graham Earley <graham.p.earley@gmail.com>
2016-04-09 00:10:47 -0500

    Began MatchMaker class for returning pairs of characters

---
+++ Unity Project/End of the Line/Assets/Decisions/MatchMaker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class MatchMaker : MonoBehaviour {
+
+	List<CharacterStruct> allCharacters;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public Pair<List<CharacterStruct>, List<CharacterStruct>> getPair() {
+		
+	}
+}
