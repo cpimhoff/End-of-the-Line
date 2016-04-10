d1f6a0f53a2bedd6542125cfd80849a0c165725d
Charlie Imhoff <cpimhoff@gmail.com>
2016-04-09 23:48:33 -0500

    A broken Tumbleweed
    meh-

---
+++ Unity Project/End of the Line/Assets/World Generation/Parts/Props/Scripts/Tumbleweed.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tumbleweed : MonoBehaviour {
+
+	public float spinRate = 1f;
+	public float moveRate = 1f;
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(new Vector3(0,(spinRate * Time.deltaTime),0));
+		this.transform.Translate (new Vector3 ((moveRate * Time.deltaTime),0,0));
+	}
+}
