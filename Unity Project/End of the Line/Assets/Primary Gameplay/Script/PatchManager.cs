d69afd7b6e92dc536ac8ded0ae748ecf06101720
Charlie Imhoff <cpimhoff@gmail.com>
2016-04-09 23:22:00 -0500

    Streaming World? Yeah.
    Great Transitions? Of course. Minimal Bugs? I sure *think* so.

--- Unity Project/End of the Line/Assets/Primary Gameplay/Script/PatchManager.cs
+++ Unity Project/End of the Line/Assets/Primary Gameplay/Script/PatchManager.cs
@@ -8,5 +8,21 @@ public class PatchManager : MonoBehaviour {
 
 	public PatchGenerator generator;	// the generator to use for creating patches
 
+	public void GeneratePatchAtAnchor(Transform anchor) {
+		GameObject patch = generator.NextPatch ();
+		patch.transform.SetParent (this.transform);
+		patch.transform.position = anchor.position;
+		patch.transform.rotation = anchor.rotation;
+		patch.SetActive (true);
+	}
+
+	public static PatchManager FindAManager() {
+		GameObject possible = GameObject.FindGameObjectWithTag ("WorldGenerator");
+		if (possible != null) {
+			return possible.GetComponent<PatchManager> ();
+		} else {
+			return null;
+		}
+	}
 
 }
