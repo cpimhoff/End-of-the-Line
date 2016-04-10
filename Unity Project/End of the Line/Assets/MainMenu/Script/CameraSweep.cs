38c268f52400d1f7767c4c2394b13c1ffc0a265f
Charlie Imhoff <cpimhoff@gmail.com>
2016-04-10 02:48:07 -0500

    Start of a Menu

---
+++ Unity Project/End of the Line/Assets/MainMenu/Script/CameraSweep.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSweep : MonoBehaviour {
+
+	public Vector3 end;
+	private Quaternion endingRotation;
+	public float duration = 5.0f;
+	private float progress = 0f;
+
+	// Use this for initialization
+	void Start () {
+		endingRotation = Quaternion.Euler (end);
+		this.enabled = false;
+	}
+
+	void Update () {
+		if (progress > duration) {
+			this.enabled = false;
+		}
+
+		progress += Time.deltaTime;
+		var alpha = Mathf.Min ((progress / duration), 1f);
+		this.transform.rotation = Quaternion.Lerp(this.transform.rotation, endingRotation, alpha);
+	}
+
+}
